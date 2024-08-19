db3b519333aebd0966b50ddb66db5709651551fb
guobao2333 <shiguobaona@163.com>
2024-08-20 00:43:50 +0800

    修复：vercel无法进行本地转发

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import fetch from 'node-fetch';
 
-const PORT = 9000;
 const app = express();
 
-export default async (req, res) => {
-  const { method, body, url } = req;
+app.all('*', (req, res) => {
+  const { method, body } = req;
+  const domain = req.get('host');
 
   // 转发请求
-  const response = await fetch(`http://localhost:${PORT}/server.js`, {
+  const response = await fetch(`https://${domain}/server.js`, {
     method,
     body: method === 'GET' ? null : JSON.stringify(body),
     headers: { 'Content-Type': 'application/json' }
@@ -17,9 +17,4 @@ export default async (req, res) => {
   // 返回响应
   const data = await response.json();
   res.status(response.status).json(data);
-};
-
-// 启动本地服务器
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
 });
